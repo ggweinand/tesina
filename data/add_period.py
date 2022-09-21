3c5db70c12c6d5446dd4a4658714a0532ea12fbf
Weheineman <gi4nn1@yahoo.de>
2022-09-20 23:14:56 -0300

    Uptated add_period. * Now uses CatalogLoader.

--- data/add_period.py
+++ data/add_period.py
@@ -1,14 +1,15 @@
-import carpyncho
 import numpy as np
 import pandas as pd
 
-client = carpyncho.Carpyncho()
-tile_list = list(client.list_tiles())[1:]
+from catalog.catalog_loader import CatalogLoader
+
+loader = CatalogLoader("../catalog")
+tile_list = loader.list_tiles()
 
 for tile in tile_list:
-    catalog = client.get_catalog(tile, "features")
+    catalog = loader.get_features(tile)
     df = pd.read_csv(f"min_obs_{tile}_snr20.csv")
-    id_list = df.bm_src_id
-    period_list = np.array([catalog[catalog["id"] == id].PeriodLS for id in id_list])
-    df = df.assign(PeriodLS=period_list)
-    df.to_csv(f"min_obs_{tile}_snr20.csv", index=False)
+    if "PeriodLS" not in df.columns:
+        period_list = np.array([catalog[catalog["id"] == id].PeriodLS for id in df.bm_src_id])
+        df = df.assign(PeriodLS=period_list)
+        df.to_csv(f"min_obs_{tile}_snr20.csv", index=False)
