660451b1e7878a23d64698c8dc4b66b254aa1ca4
Gianni Georg Weinand <gi4nn1@yahoo.de>
2022-11-28 22:17:29 -0300

    Added period mse script and plots.

--- plot/subplot_period_synth_mse.py
+++ plot/subplot_period_synth_mse.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import sys
 
 from catalog.catalog_loader import CatalogLoader
+
+gp_lib = sys.argv[1]
 
 fig, axs = plt.subplots(6, 3, sharey=True)
 loader = CatalogLoader("../catalog")
 tile_list = loader.list_tiles()
-# The data goes from 0 to 40 synthetic points with a step size of 2.
-n_steps = 20 + 1
+# The data goes from 0 to 200 synthetic points with a step size of 2.
+n_steps = 100 + 1
 
 for ax, tile in zip(axs.flatten(), tile_list):
-    df = pd.read_csv(f"../data/period_synth_george_{tile}.csv")
+    df = pd.read_csv(f"../data/period_synth_{gp_lib}_{tile}.csv")
 
     squared_err = np.zeros(n_steps)
     count = np.zeros(n_steps)
@@ -19,7 +22,7 @@ for ax, tile in zip(axs.flatten(), tile_list):
         period_catalog = df.loc[
             (df["id"] == id) & (df["n_synth"] == 0), "period_ls"
         ].values[0]
-        for idx, n_synth in enumerate(np.linspace(0, 40, n_steps)):
+        for idx, n_synth in enumerate(np.linspace(0, 200, n_steps)):
             period_row = df.loc[
                 (df["id"] == id) & (df["n_synth"] == n_synth), "period_ls"
             ].values
@@ -41,12 +44,13 @@ for ax, tile in zip(axs.flatten(), tile_list):
     # )
     # ax.set_xlim(left=-2)
     # ax.set_xlim(right=300)
-    ax.set_ylim((10**-4, 10**4))
+    ax.set_ylim((10**-6, 10**2))
     ax.set_xlabel("Points added by Gaussian Process")
     ax.set_ylabel("Period MSE")
     ax.set_yscale("log")
     ax.set_title(tile)
 
-fig.suptitle(f"Using GP from george")
+fig.suptitle(f"Using GP from {gp_lib}")
+fig.tight_layout()
 
 plt.show()
